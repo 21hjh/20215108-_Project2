c510e258f960d223e03f35ab878273a1e14eb419
21 hjh <ghdwoghk04@sch.ac.kr>
2022-10-06 11:44:41 +0900

    최종

---
+++ 20215108홍재화_Project2/Ex_01.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+
+	//P.115~ 1번문제
+
+	string x, y;
+
+	cout << "첫 번째 문자열: ";
+	cin >> x;
+
+	cout << "두 번째 문자열: ";
+	cin >> y;
+
+	if (x == y)
+		cout << "2개의 문자열은 같습니다." << endl;
+	else
+		cout << "2개의 문자열은 다릅니다." << endl;
+
+	return 0;
+
+
+	//P.115~ 10번문제
+
+
+	for (int x = 1; x < 100; x++)
+		for (int y = 1; y < 100; y++)
+			for (int z = 1; z < 100; z++)
+				if (pow(x, 2) + pow(y, 2) == pow(z, 2))
+					cout << x << " " << y << " " << z << endl;
+
+	return 0;
+
+
+
+	//P.115~ 16번문제
+
+	int money = 50;
+	const int goal = 250;
+
+	int bets, win;
+	bool result;
+
+	int wins = 0;
+	int total = 0;
+
+	srand((unsigned int)time(NULL));
+
+	cout << "초기 금액 $" << money << endl;
+	cout << "목표 금액 $" << goal << endl;
+
+	for (int game = 0; game < 1000; game++)
+	{
+		money = 50, bets = 0, win = 0;
+		while (true)
+		{
+			bets++;
+			if ((double)rand() / RAND_MAX < 0.5)
+			{
+				money++;
+				win++;
+			}
+			else
+				money--;
+
+			if (money <= 0)
+			{
+				result = false;
+				break;
+			}
+			if (money >= goal)
+			{
+				result = true;
+				break;
+			}
+		}
+
+		if (result == true)
+			wins++;
+		total += bets;
+	}
+
+	cout << "1000 중의 " << wins << "번 승리" << endl;
+	cout << fixed;
+	cout.precision(6);
+	cout << "이긴 확률=" << wins / 1000.0 * 100.0 << endl;
+	cout << "평균 배팅 횟수 = " << total / 1000.0 << endl;
+
+	return 0;
+
+}
